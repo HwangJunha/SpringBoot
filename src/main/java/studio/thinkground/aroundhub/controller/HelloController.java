5be1c661d6f225647d406094f8fa9f9defb22dda
HwangJunha <45688526+HwangJunha@users.noreply.github.com>
2021-12-22 22:25:10 +0900

    postapi: dto, map

---
+++ src/main/java/studio/thinkground/aroundhub/controller/HelloController.java
@@ -0,0 +1,16 @@
+package studio.thinkground.aroundhub.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping(value="/hello")
+    public String hello(){
+        return "Hello World";
+    }
+    
+    
+
+}
