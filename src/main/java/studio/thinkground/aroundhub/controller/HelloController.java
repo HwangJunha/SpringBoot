a8864af2fb0d5338721fb03cd697a41445fd4fbf
HwangJunha <45688526+HwangJunha@users.noreply.github.com>
2021-12-29 22:37:36 +0900

    로그 학습

--- src/main/java/studio/thinkground/aroundhub/controller/HelloController.java
+++ src/main/java/studio/thinkground/aroundhub/controller/HelloController.java
@@ -1,16 +1,28 @@
 package studio.thinkground.aroundhub.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     
+    private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping(value="/hello")
     public String hello(){
         return "Hello World";
     }
     
+    @RequestMapping("log-test")
+    public void logTest(){
+        LOGGER.trace("trace");
+        LOGGER.info("info");
+        LOGGER.debug("debug");
+        LOGGER.warn("warn");
+        LOGGER.error("error");
+    }
     
 
 }
--- src/main/java/studio/thinkground/aroundhub/controller/ProductController.java
+++ src/main/java/studio/thinkground/aroundhub/controller/ProductController.java
@@ -1,5 +1,8 @@
 package studio.thinkground.aroundhub.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,7 @@ import studio.thinkground.aroundhub.service.ProductService;
 @RestController
 @RequestMapping("/api/v1/product-api")
 public class ProductController {
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     private ProductService productService;
 
     @Autowired
@@ -22,7 +26,13 @@ public class ProductController {
 
     @GetMapping(value = "/product/{productId}")
     public ProductDTO getProduct(@PathVariable String productId){
-        return productService.getProductDTO(productId);
+        
+        long startTime = System.currentTimeMillis();
+        LOGGER.info("[ProductController] perform {} of Around Hub API.", "getPorduct");
+        ProductDTO productDto=productService.getProductDTO(productId);
+        LOGGER.info("[ProductController] Response :: productId = {}, productName = {}, productPirce = {}, productStock = {}, Response Time = {}ms", 
+        productDto.getProductId(), productDto.getProductName(), productDto.getProductPrice(), productDto.getProductStock(), (System.currentTimeMillis()-startTime));
+        return productDto;
     }
 
     @PostMapping(value="/product")
